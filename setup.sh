402c4acf39a39d593a2c2d0760aa660e349522dd
otix <otix@yoga14s>
2025-07-27 22:40:21 +0800

    移除跨平台安装。这玩意没用

--- setup.sh
+++ setup.sh
@@ -2,14 +2,6 @@
 
 source ./utils.sh
 
-if [[ $TARGET_OS == unknown ]]; then
-	raise_error "unsupport platform"
-fi
-
-echo
-echo OS = $TARGET_OS
-echo
-
 do_sub_scripts . setup.sh $*
 
 echo finish setup.
--- utils.sh
+++ utils.sh
@@ -1,9 +1,68 @@
 #!/bin/bash
 
-# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
-cur_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source $cur_dir/util_base.sh
-source $cur_dir/util_commands.sh
-source $cur_dir/util_mirror.sh
-source $cur_dir/util_package.sh
+# 移除目录
+function RM(){
+	# dirs
+	if [[ $# -lt 1 ]]; then
+		return
+	fi
+	echo "Removing $* ..."
 
+	rm -rf $*
+}
+
+# 新建目录
+function MKDIR(){
+	# dirs
+	if [[ $# -lt 1 ]]; then
+		return
+	fi
+	echo "Making directory $* ..."
+
+	mkdir -p $*
+}
+
+# 复制目录
+function CP(){
+	# from ... to
+	if [[ $# -lt 2 ]]; then
+		return
+	fi
+	echo "Copying to ${!#} ..."
+	cp -rfb $*
+}
+
+
+# 执行子目录脚本，子目录可选
+# path, script_name, select sub dirs
+function do_sub_scripts() {
+	if [[ $# -lt 2 ]]; then
+		raise_error "do_sub_scripts() need st least 2 arguments"
+	else
+		local path=$1
+		local script_name=$2
+		shift 2
+		local select_set=${*:-$(ls $path)}
+	fi
+
+	for dir in $select_set; do
+		if [[ -d $path/$dir && -f $path/$dir/$script_name ]]; then
+			echo ' ==> ' $path/$dir/$script_name
+			echo
+			cd $path/$dir
+			bash $script_name
+			local co=$?
+			if [[ $co -ne 0 ]]; then
+				raise_error "$path/$dir/$script_name" exit with code: $co
+			fi
+			cd - &>/dev/null
+			echo
+		fi
+	done
+}
+
+# 父目录名
+function locate_dir(){
+	local path=$(pwd)
+	echo ${path##*/}
+}
