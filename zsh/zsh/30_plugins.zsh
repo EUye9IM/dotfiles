84cd677fcbec7bf6efcb2993efdbdd6d32322004
otix <otix@yoga14s>
2025-11-05 01:02:54 +0800

    zdh: add fzf-tab

--- zsh/zsh/30_plugins.zsh
+++ zsh/zsh/30_plugins.zsh
@@ -6,3 +6,6 @@ zinit ice depth"1"
 zinit light romkatv/powerlevel10k
 zinit ice depth"1"
 zinit light zsh-users/zsh-completions
+if command -v fzf >/dev/null 2>&1; then
+zinit light Aloxaf/fzf-tab
+fi
