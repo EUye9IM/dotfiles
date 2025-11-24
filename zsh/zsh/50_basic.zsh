2db36c7eaa478b0fec22494460ea958ee1df0fcf
otix <otix@yoga14s>
2025-11-24 12:17:29 +0800

    zsh: set correct

--- zsh/zsh/50_basic.zsh
+++ zsh/zsh/50_basic.zsh
@@ -2,7 +2,7 @@
 HISTFILE=~/.histfile
 HISTSIZE=1000
 SAVEHIST=1000
-setopt autocd beep extendedglob no_nomatch notify
+setopt autocd beep extendedglob no_nomatch notify correct_all
 bindkey -e
 # enable shift tab
 bindkey '^[[Z' reverse-menu-complete
