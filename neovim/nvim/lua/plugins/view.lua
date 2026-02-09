5f63cfd284f43e609fbd1f7d6e016b0a2a222876
otix <otix@yoga14s>
2026-02-09 23:53:05 +0800

    add git blame

--- neovim/nvim/lua/plugins/view.lua
+++ neovim/nvim/lua/plugins/view.lua
@@ -10,8 +10,40 @@ return {
 	},
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {},
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			{
+				"f-person/git-blame.nvim",
+				opts = {
+					message_template = "<sha> - <author> (<date>) <summary>", -- template for the blame message, check the Message template section for more options
+					date_format = "%Y-%m-%d %H:%M:%S", -- template for the date, check Date format section for more options
+					virtual_text_column = 0,
+				},
+			},
+		},
+		config = function()
+			local git_blame = require("gitblame")
+			vim.g.gitblame_display_virtual_text = 0
+			vim.g.gitblame_delay = 10
+			require("lualine").setup({
+				sections = {
+					lualine_c = {
+						"filename",
+						{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+					},
+				},
+			})
+		end,
+	},
+	{
+		"f-person/git-blame.nvim",
+		event = "VeryLazy",
+		opts = {
+			enabled = true, -- if you want to enable the plugin
+			-- message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+			-- date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+			-- virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+		},
 	},
 	{ "dstein64/nvim-scrollview", opts = {} },
 	{
