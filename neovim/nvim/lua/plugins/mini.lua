e3718c9d46aee0b7efaf1abf307cf8382cfc329c
otix <otix@yoga14s>
2026-06-07 23:31:53 +0800

    feat(neovim): 安装 mini.ai + mini.indentscope（参数/缩进文本对象和跳转）

---
+++ neovim/nvim/lua/plugins/mini.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"echasnovski/mini.ai",
+		version = false,
+		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		"echasnovski/mini.indentscope",
+		version = false,
+		event = "VeryLazy",
+		opts = {
+			draw = {
+				delay = 100,
+				animation = function() end,
+			},
+			symbol = "│",
+			options = { try_as_border = true },
+		},
+	},
+}
