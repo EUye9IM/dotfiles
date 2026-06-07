6a4371567dcf3eac04853ce059cfb00341701ce9
otix <otix@yoga14s>
2026-06-07 23:30:35 +0800

    feat(neovim): 添加 Zed 风格 Git hunk 键映射（gitsigns.nvim）

--- neovim/nvim/lua/plugins/git.lua
+++ neovim/nvim/lua/plugins/git.lua
@@ -2,6 +2,39 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		opts = {},
+		keys = {
+			{
+				"]c",
+				function()
+					require("gitsigns").nav_hunk("next", { target = "all" })
+				end,
+				desc = "下一个变更",
+			},
+			{
+				"[c",
+				function()
+					require("gitsigns").nav_hunk("prev", { target = "all" })
+				end,
+				desc = "上一个变更",
+			},
+			{ "do", "<cmd>Gitsigns preview_hunk<cr>", desc = "展开 hunk" },
+			{ "dp", "<cmd>Gitsigns reset_hunk<cr>", desc = "恢复 hunk" },
+			{ "dU", "<cmd>Gitsigns stage_hunk<cr>", desc = "暂存 hunk" },
+			{
+				"du",
+				function()
+					require("gitsigns").undo_stage_hunk()
+				end,
+				desc = "取消暂存 hunk",
+			},
+			{
+				"dO",
+				function()
+					require("gitsigns").stage_hunk()
+				end,
+				desc = "切换暂存状态",
+			},
+		},
 	},
 	{
 		"f-person/git-blame.nvim",
