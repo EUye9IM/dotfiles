8206b8e6caaa47a067d9442a025781b9d0f839c9
otix <otix@yoga14s>
2026-06-07 23:34:32 +0800

    feat(neovim): 启用 Tree-sitter textobjects（af/if/ac/ic 文本对象，]m/[m 跳转）

--- neovim/nvim/lua/plugins/treesitter.lua
+++ neovim/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,10 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		dependencies = { "nvim-treesitter/nvim-treesitter-context" },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-context",
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
 		branch = "main",
 		lazy = false,
 		build = ":TSUpdate",
@@ -18,6 +21,40 @@ return {
 					end
 				end,
 				additional_vim_regex_highlighting = true,
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = "@class.outer",
+						["]/"] = "@comment.outer",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+						["[/"] = "@comment.outer",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
+					},
+				},
 			},
 		},
 		config = function(_, opts)
